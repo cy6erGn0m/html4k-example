f574eeba6507154146e3d90e7129cf47e3ec2f84
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2015-04-13 15:18:29 +0300

    Better test for up down

--- clientside/src/test/kotlin/quotes-table.kt
+++ clientside/src/test/kotlin/quotes-table.kt
@@ -5,15 +5,21 @@ import org.mockito.Matchers
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.*
+import java.util.ArrayList
 import org.junit.Test as test
 
 class QuotesTableTest {
 
 
-    test fun moveUp() {
+    test fun moveUpDown() {
         val view = mock(javaClass<QuotesTableViewModel>())
+        val createdRows = ArrayList<QuoteRowViewModel>()
+
         `when`(view.createRowModel()).thenAnswer {
-            mock(javaClass<QuoteRowViewModel>())
+            val row = mock(javaClass<QuoteRowViewModel>())
+            createdRows.add(row)
+
+            row
         }
 
         val presenter = QuotesTablePresenter(view)
@@ -21,6 +27,24 @@ class QuotesTableTest {
 
         verify(view, times(1)).createRowModel()
         verify(view, times(1)).appendRowModel(any())
+        verify(createdRows.single(), times(1)).setInstrumentName("A")
+        verify(createdRows.single(), times(1)).setValue(1.0)
+        verify(createdRows.single(), times(1)).setMove(QuoteMove.NEUTRAL)
+
+        presenter.onQuote("A", 1.5)
+        verify(createdRows.single(), times(1)).setInstrumentName("A")
+        verify(createdRows.single(), times(1)).setValue(1.5)
+        verify(createdRows.single(), times(1)).setMove(QuoteMove.UP)
+
+        presenter.onQuote("A", 0.5)
+        verify(createdRows.single(), times(1)).setInstrumentName("A")
+        verify(createdRows.single(), times(1)).setValue(0.5)
+        verify(createdRows.single(), times(1)).setMove(QuoteMove.DOWN)
+
+        // verify totals
+        verify(createdRows.single(), times(1)).setInstrumentName(any())
+        verify(createdRows.single(), times(3)).setValue(Matchers.anyDouble())
+        verify(createdRows.single(), times(3)).setMove(any())
     }
 
     suppress("NOTHING_TO_INLINE")
